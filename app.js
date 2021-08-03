e6f30497e680140056eef18c1914684ee01c4238
Juan Diaz <juanferreira65@gmail.com>
2021-08-03 19:29:37 -0400

    modificando header para ser responsive

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 
 let btn_menu = document.querySelector(".btn__menu");
 let nav_mobile = document.querySelector(".nav__links--mobile");
+let links = nav_mobile.getElementsByClassName("nav__link");
 
 btn_menu.addEventListener("click",()=>{
-
-    if ( !btn_menu.classList.contains("visible") ){
-        btn_menu.classList.toggle("visible");
-        nav_mobile.classList.toggle("visible");
-    }else{
-        btn_menu.classList.toggle("visible");
-        nav_mobile.classList.toggle("visible");
-    }
-
+    agregarVisible(btn_menu,nav_mobile);
 });
+
+for( let i = 0; i < links.length ; i++ ){
+    links[i].addEventListener("click",()=>{
+        agregarVisible(btn_menu,nav_mobile);
+    });
+}
+
+function agregarVisible( ...elementos){
+    for( let i = 0; i < elementos.length ; i++ ){
+        elementos[i].classList.toggle("visible");
+    }
+}
